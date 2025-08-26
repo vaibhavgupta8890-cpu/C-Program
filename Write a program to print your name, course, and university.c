049e5dd75c440db4bd2b5764f351728367d9b3e3
vaibhavgupta8890-cpu <vaibhavgupta8890@gmail.com>
2025-08-26 15:22:33 +0530

    Create Write a program to print your name, course, and university.c

---
+++ Write a program to print your name, course, and university.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ int main() {
+    
+    char name[] = "vaibhav";
+    char course[] = "btech ";
+    char university[] = " chandigardh University";
+
+   
+    printf("Name      : %s\n", name);
+    printf("Course    : %s\n", course);
+    printf("University: %s\n", university);
+
+    return 0;
+}
